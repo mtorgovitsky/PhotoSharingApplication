cfd5f8cff09dc26e5ca4bcecdd1c63fb8e9c6b79
Michael Torgovitsky <m.torgovitsky@gmail.com>
2017-03-15 10:31:10 +0200

    Add project files.

---
+++ PhotoSharingApplication/Models/PhotoSharingInitializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PhotoSharingApplication.Models
+{
+    public class PhotoSharingInitializer : DropCreateDatabaseAlways<PhotoSharingContext>
+    {
+        protected override void Seed(PhotoSharingContext context)
+        {
+            List<Photo> photos = new List<Photo>()
+            {
+                new Photo
+                {
+                    Title="Test Photo",
+                    Description = "Some Description",
+                    UserName = "NaokiSato",
+                    PhotoFile = getFileBytes
+                }
+            };
+        }
+    }
+}
